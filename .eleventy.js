7edc2cdc8a589b95667106db0aed2da094886643
GitHub Action <action@github.com>
2026-06-06 13:38:39 -0600

    ss

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+
+  eleventyConfig.addFilter("dateDisplay", function(dateObj) {
+    return new Date(dateObj).toLocaleDateString("en-US", {
+      year: "numeric", month: "long", day: "numeric", timeZone: "UTC"
+    });
+  });
+
   return {
     dir: {
       input: "src",
